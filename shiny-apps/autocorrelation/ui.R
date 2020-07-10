53b165ada6cb1cc68215df9034e301402df235f5
JFeldbauer <johannes.feldbauer@posteo.de>
2020-07-10 11:52:27 +0200

    added autocorrelation shiny app

---
+++ shiny-apps/autocorrelation/ui.R
@@ -0,0 +1,33 @@
+library("rhandsontable")
+library("plotly")
+
+
+shinyUI(fluidPage(
+  headerPanel("Autocorrelation"),
+  sidebarLayout(
+    sidebarPanel(
+      
+      h3("Input data"),
+      
+      rHandsontableOutput("hot"),
+      checkboxInput("arrow", "show shift", FALSE),
+      h4("Lag"),
+      sliderInput("lag", "Autocorrelcation lag", min = 0, max = 15, value = 0),
+      h4("Actions"),
+      actionButton("addRows", "+10 rows"),
+      actionButton("clrBtn", "Clear")
+      
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plots",
+                plotOutput("plot"),
+                plotOutput("plot2"),
+                plotOutput("plot3")
+                 #verbatimTextOutput("summary")
+        )
+        )
+      )
+    )
+  )
+)
